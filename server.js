d4e1e25071fd1c89cd1accbbfff556be079286a8
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-12-03 11:20:46 +0530

    Add dotenv and morgan dependencies

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const {setupLogging} = require("./src/middleware/logging");
+
 
 const app = express()
 const port = 3000;
 
 
+setupLogging(app);
 app.get('/hello', (req, resp) => {
     return resp.send('HELLO WORLD!');
 })
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
